c17a25b6d5efbef56a43baa2579e1b204ee6bc13
Christopher Nielsen <Nillerduden@hotmail.com>
2020-02-08 04:06:24 +0100

    Initial Project with BaseViewModel
    Created BaseViewModel with propertychanged functionality.

---
+++ DnDSpellsCompendium/DnDSpellsCompendium/ViewModels/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+
+namespace DnDSpellsCompendium.ViewModels
+{
+	[AddINotifyPropertyChangedInterface]
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+	}
+}
